46ab9319189f051e7cf2a0b41b84da00cd0e3627
Andres Jaramillo <JaramilloG1999@outlook.com>
2024-04-25 06:23:33 -0500

    feat: update structure app

--- app.ts
+++ app.ts
@@ -6,20 +6,10 @@ import PostgreSQL from './src/config/postgresql';
 
 const port = process.env.PORT || 3001;
 const app = express();
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.static('storage'));
 app.use('/api', router);
-app.use((error: any, req: any, res: any) => {
-  console.error(error);
-  res.status(error.status || 500).json({
-    message: error.message || 'An unexpected error occurred',
-  });
-});
 
 PostgreSQL.getInstance()
   .then(() => {
